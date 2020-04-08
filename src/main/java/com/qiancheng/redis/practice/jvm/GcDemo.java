838a832d1f65edbd823e386579d7e2c7544b955d
qiancheng <qiancheng@shuidihuzhu.com>
2020-04-09 00:52:33 +0800

    gc print

---
+++ src/main/java/com/qiancheng/redis/practice/jvm/GcDemo.java
@@ -0,0 +1,20 @@
+package com.qiancheng.redis.practice.jvm;
+
+/**
+ * -verbose:gc -XX:+PrintGCDetails
+ */
+public class GcDemo {
+    public Object instance = null;
+    private static final int num = 1024 * 1024;
+    private byte[] bigSize = new byte[2 * num];
+
+    public static void main(String[] args) {
+        GcDemo gcDemo1 = new GcDemo();
+        GcDemo gcDemo2 = new GcDemo();
+        gcDemo1.instance = gcDemo2;
+        gcDemo2.instance = gcDemo1;
+        gcDemo1 = null;
+        gcDemo2 = null;
+        System.gc();
+    }
+}
