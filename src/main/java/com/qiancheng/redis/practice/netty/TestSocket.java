6a2f648954183e750cfdba41200e4eb10089c413
qiancheng <qiancheng@shuidihuzhu.com>
2020-04-18 22:16:57 +0800

    netty

--- src/main/java/com/qiancheng/redis/practice/netty/TestSocket.java
+++ src/main/java/com/qiancheng/redis/practice/netty/TestSocket.java
@@ -29,7 +29,7 @@ public class TestSocket {
                     }
                 }).start();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
